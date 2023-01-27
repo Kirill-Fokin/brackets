97ff51618b2d544ececa5467f3dc69c370fb0877
Kirill-Fokin <kirill19011998@mail.ru>
2023-01-28 01:59:03 +0300

    start tsk

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  // yourvar result;
+	if (str.length % 2 !== 0)
+  result = false;
+while (1) {
+let oldlength = str.length;
+for (let i = 0; i < bracketsConfig.length; i++) {
+str = str.replace(bracketsConfig[i].join(''), '');
 }
+if (str.length === oldlength || str.length === 0) {
+break;
+}
+}
+if(str.length === 0)
+  result = true;
+else 
+  result = false;
+return result;
+} 
+  
+  
+  
+
